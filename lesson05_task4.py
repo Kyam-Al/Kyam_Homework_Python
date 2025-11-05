0e5c67b5eda7de7d08fd3d2bc721dafd5480df93
Kyam-Al <nikolay-aliev1994@mail.ru>
2025-11-06 01:15:55 +0400

    Add files via upload

---
+++ lesson05_task4.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Firefox()
+
+try:
+    driver.get("http://the-internet.herokuapp.com/login")
+
+    username_field = driver.find_element(By.ID, "username")
+    username_field.send_keys("tomsmith")
+
+    password_field = driver.find_element(By.ID, "password")
+    password_field.send_keys("SuperSecretPassword!")
+
+    login_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    login_button.click()
+
+    time.sleep(2)
+
+    success_message = driver.find_element(By.ID, "flash")
+    message_text = success_message.text
+
+    print(message_text)
+
+except Exception as e:
+    print(f"Произошла ошибка: {e}")
+
+finally:
+    driver.quit()
